bf7c35e9f714f5ba7536d941b6bf54a1f77f25e2
sim236 <97866700+sim236@users.noreply.github.com>
2022-07-08 04:14:11 +0530

    added Check point 7 and Code Refector

--- src/main/scala/Model/Model.scala
+++ src/main/scala/Model/Model.scala
@@ -7,11 +7,12 @@ object Parent {
   case class ClassifyData(data:ListBuffer[ListBuffer[String]])
   case class ClassifiedObjectData(order:PurchaseDetail)
   case class ErrorHandling(data:Any,Error:Exception)
+  case class Combine(data:PurchaseDetail)
 }
 object Child{
   case class ClassifyDataChild(data: ListBuffer[String])
   case class StreamDataChannel(data:ListBuffer[String])
   case class CategoryFilterFlow(data :ListBuffer[PurchaseDetail])
   case class CategoryWiseFinancialYear(data:ListBuffer[PurchaseDetail])
-  case class Combine(data:PurchaseDetail)
+  case class BulkProductInsights(data:ListBuffer[PurchaseDetail])
 }
